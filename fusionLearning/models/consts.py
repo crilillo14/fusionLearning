ae414d59ef16cc0028aaa8c9e7f8e4245e1ad82d
crilillo14 <crilillo2015@gmail.com>
2025-12-08 15:16:00 +0000

    train val test infer vis orchestrator

--- fusionLearning/models/consts.py
+++ fusionLearning/models/consts.py
@@ -1,8 +1,8 @@
 """Control variables for base model arch and optimizers"""
 
 
-MAXEPOCHS : int = 10
-BATCHSIZE : int = 10
+MAXEPOCHS : int = 20
+BATCHSIZE : int = 8 
 MOMENTUM : float = 0.99
 LEARNING_RATE : float = 0.01
 
